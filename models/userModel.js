8fa5c9a23af9e8154458505ef72e50422b840813
PratikGarai <pratikgarai0208@gmail.com>
2021-11-12 11:18:13 +0530

    Added user model

---
+++ models/userModel.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    isAdmin: {
+        type: Boolean,
+        required: true,
+        default: false
+    }
+}, {
+    timestamps: true
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    const res = await bcrypt.compare(enteredPassword, this.password);
+    return res;
+}
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
