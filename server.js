0ecd6bbba7a37d26fbe0b333ed80a4e8669e29b5
PratikGarai <pratikgarai0208@gmail.com>
2021-11-12 12:02:23 +0530

    Middleware and auth routes checked

--- server.js
+++ server.js
@@ -25,4 +25,4 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}...`.yellow.bold));
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV||"development"} mode on port ${PORT}...`.yellow.bold));
